1a80b3b2f19428d98cd11480b987bb1452ee0d70
EnderMachineBr <o.lima1@outlook.com.br>
2024-03-13 20:42:31 -0300

    versao 1.5
    Melhoramento no menu.
    Adição de novas pastas e caminhos.
    Alteração código de impressão de termo.

--- src/confGerarPdf.js
+++
@@ -1,7 +0,0 @@
-function printArea (areaImpressao){
-    var printdata=document.getElementById("areaImpressao");
-    newwin=window.open("");
-    newwin.document.write(printdata.outerHTML);
-    newwin.print();
-    newwin.close();
-}
